4deaeccb595c00941cbd2bbb574bcad70d0af385
Dean Ferreira <deanferreira20@gmail.com>
2024-01-23 23:08:12 -0600

    Add util functions

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+function validateID(courseInfoID, agCourseID) {}
+
+function validateSubmissionDate(submissionDateString, dueDateString) {}
+
+function calcAvgScore(leanerProfile) {}
+
+function updateAvgScore(leanerProfile) {}
+
+module.exports = {
+    validateID,
+    validateSubmissionDate,
+    calcAvgScore,
+    updateAvgScore,
+};
