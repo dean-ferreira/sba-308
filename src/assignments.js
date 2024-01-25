00c1a8c1bfbff459785ba76999197c3b750ef865
Dean Ferreira <deanferreira20@gmail.com>
2024-01-24 18:04:46 -0600

    Implement assignment functions

--- src/assignments.js
+++ src/assignments.js
@@ -1,8 +1,19 @@
 const { isDateBefore } = require('./utils');
 
-function getAssignmentsDue(assignments) {}
+function getAssignmentsDue(assignments) {
+    const assignmentsDue = [];
+    for (const assignmentID in assignments) {
+        let currentDate = new Date().toISOString().slice(0, 10);
+        if (isDateBefore(assignments[assignmentID].due_at, currentDate)) {
+            assignmentsDue.push(assignments[assignmentID]);
+        }
+    }
+    return assignmentsDue;
+}
 
-function getLearnerSubmissions(learnerProfile) {}
+function getLearnerSubmissions(learnerProfile) {
+    return learnerProfile.learner_submissions;
+}
 
 module.exports = {
     getAssignmentsDue,
