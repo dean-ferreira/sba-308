9868c3897656a7c434fd6988d14b0ae34fe9ddb3
Dean Ferreira <deanferreira20@gmail.com>
2024-01-24 20:27:02 -0600

    Refactor assignment functions into utils

--- src/utils.js
+++ src/utils.js
@@ -10,6 +10,21 @@ function calcTotalPointsDue(assignmentsDue) {}
 
 function calcTotalPointsEarned(assignmentsDue, learnerSubmissions) {}
 
+function getAssignmentsDue(assignments) {
+    const assignmentsDue = [];
+    for (const assignmentID in assignments) {
+        let currentDate = new Date().toISOString().slice(0, 10);
+        if (isDateBefore(assignments[assignmentID].due_at, currentDate)) {
+            assignmentsDue.push(assignments[assignmentID]);
+        }
+    }
+    return assignmentsDue;
+}
+
+function getLearnerSubmissions(learnerProfile) {
+    return learnerProfile.learner_submissions;
+}
+
 module.exports = {
     validateID,
     isDateBefore,
@@ -17,4 +32,6 @@ module.exports = {
     updateAvgScore,
     calcTotalPointsDue,
     calcTotalPointsEarned,
+    getAssignmentsDue,
+    getLearnerSubmissions,
 };
